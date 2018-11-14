02fe4918c0915e7fd1e963f3b05cf076984188ad
sergii <sergiishevchenko017@gmail.com>
2018-11-14 20:52:21 +0300

    first commit

--- main.py
+++ main.py
@@ -3,10 +3,35 @@ import constants
 
 bot = telebot.TeleBot(constants.token)
 
-bot.send_message(486128297, "test")
+# bot.send_message(486128297, "test")
 # 48612897 - my id
+#
+# upd = bot.get_updates()
+# print(upd)
+# last_upd = upd[-1]
+# message_from_user = last_upd.message
+# print(message_from_user)
 
-upd = bot.get_updates()
-print(upd)
-last_upd = upd[-1]
-message_from_user = last_upd.message
+
+@bot.message_handler(content_types=["commands"])
+def handle_command(message):
+    print("Пришла команда")
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    print("Пришло простое сообщение")
+
+
+@bot.message_handler(content_types=["document"])
+def handle_document(message):
+    print("Пришёл документ")
+
+
+@bot.message_handler(content_types=["audio"])
+def handle_audio(message):
+    print("Пришла аудиозапись")
+
+
+
+bot.polling(none_stop=True, interval=0)
